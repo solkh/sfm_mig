8d4756eaa920212e9edc7bc586d6fd67824cd312
sol299 <info@sol-gr.com>
2025-05-30 16:14:08 +0300

    Refactor processAndAttachImage to remove redundant database insert for attachment, improving code clarity and maintainability.

--- mongodb-to-wp-migration.php
+++ mongodb-to-wp-migration.php
@@ -709,18 +709,6 @@ function processAndAttachImage($imageUrl, $postId, $language, $postDate)
 
         // Insert attachment
         $wpdb->query("INSERT INTO {$config['wp_prefix']}posts 
-            (post_title, post_content, post_excerpt, post_mime_type, post_status, post_type, post_parent, guid) 
-            VALUES 
-            ('{$wpdb->escape_string($attachment['post_title'])}', 
-             '{$attachment['post_content']}', 
-             '{$attachment['post_excerpt']}', 
-             '{$attachment['post_mime_type']}', 
-             '{$attachment['post_status']}', 
-             'attachment', 
-             $postId, 
-             '{$attachment['guid']}')");
-
-        $wpdb->query("INSERT INTO {$config['wp_prefix']}posts 
             (post_title, post_content, post_excerpt, to_ping, pinged, post_content_filtered, post_name, post_date, post_date_gmt, 
              post_modified, post_modified_gmt, post_status, post_author, 
              post_type, comment_status, ping_status, post_mime_type, post_parent, guid)
